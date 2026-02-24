628bdfaa85777b164207f5219f12fb932bcae512
jason2000-cpu <jasonmwangi2000@gmail.com>
2026-02-24 13:46:22 +0300

    feat: added create user account feature, added a readme file to explain every endpoint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Hash;
 
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -44,6 +46,32 @@ class UserController extends Controller
         ];
 
         return response()->json($response);
+    }
+
+    public function createAccount(Request $request) {
+        $validate = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => ['required', 'string', Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'first_name'=> $request->first_name,
+            'last_name'=> $request->last_name,
+            'email'=> $request->email,
+            'password'=> Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message'=> 'User Created Successfully',
+            'user' => [
+                'id' => $user->id,
+                'first_name'=> $user->first_name,
+                'last_name'=> $user->last_name,
+                'email' => $user->email,
+            ]
+        ], 201);
     }
 }
 
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,7 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return response()->json([
-        'message' => 'Hello Jackson 👋'
-    ]);
-});
 
 Route::get('/user/{id}', [UserController::class, 'userProfile']);
 Route::get('/users', [UserController::class, 'users']);
+Route::post('/user', [UserController::class,'createAccount']);
